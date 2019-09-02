135924190c4a43292e55f6e230fb5463e2b8d9b1
Joe <44826941+chuzcjoe@users.noreply.github.com>
2019-09-02 12:50:40 -0400

    Update 78_Subsets.py

--- 78_Subsets.py
+++ 78_Subsets.py
@@ -11,5 +11,40 @@ class Solution(object):
             
         for elem in nums:
             output.extend([x + [elem] for x in output])
+      
             
         return output
+    
+        """
+        class Solution(object):
+    def subsets(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+       
+        """
+        output = [[]]
+        
+        if not nums:
+            return output
+            
+        for elem in nums:
+            print(elem)
+            output.extend([x + [elem] for x in output])
+            print(output)
+        return output
+        """
+        res = []
+        self.dfs(nums, 0, res, [])
+        return res
+    
+        def dfs(self, nums, index, res, path):
+            print(res)
+            res.append(path)
+            for i in xrange(index, len(nums)):
+                self.dfs(nums, i + 1, res, path + [nums[i]])
+
+
+            
+        """
