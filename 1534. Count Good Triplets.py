2a50fd695bc8ca10624e28ff95e0c9ce6847cda1
Joe <44826941+chuzcjoe@users.noreply.github.com>
2020-08-03 16:54:32 -0400

    Brute Force

---
+++ 1534. Count Good Triplets.py
@@ -0,0 +1,14 @@
+class Solution:
+    def countGoodTriplets(self, arr: List[int], a: int, b: int, c: int) -> int:
+        
+        cnt = 0
+        n = len(arr)
+        
+        for i in range(n):
+            for j in range(i+1,n):
+                for k in range(j+1, n):
+                    if abs(arr[i]-arr[j]) <= a and abs(arr[j]-arr[k]) <= b and abs(arr[i]-arr[k]) <= c:
+                        cnt += 1
+        
+        
+        return cnt
