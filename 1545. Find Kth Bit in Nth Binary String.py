d38829ed722b9619374835394186d18da4e11d00
Joe <44826941+chuzcjoe@users.noreply.github.com>
2020-08-12 22:32:12 -0400

    Recursive

--- 1545. Find Kth Bit in Nth Binary String.py
+++ 1545. Find Kth Bit in Nth Binary String.py
@@ -1,6 +1,8 @@
 class Solution:
     def findKthBit(self, n: int, k: int) -> str:
         
+        """
+        #1. Brute Force
         S = "0"
         
         for i in range(2,n+1):
@@ -11,3 +13,23 @@ class Solution:
             S = S + "1" + invert[::-1]
         
         return S[k-1]
+        
+        """
+        
+        #2. recursive based on symmetry
+        
+        if n == 1:
+            return "0"
+        
+        length = 2**n - 1
+        if k == length // 2 + 1:
+            return "1"
+        
+        elif k <= length//2:
+            return self.findKthBit(n-1,k)
+        
+        else:
+            if self.findKthBit(n-1, length-k+1) == "0":
+                return "1"
+            else:
+                return "0"
